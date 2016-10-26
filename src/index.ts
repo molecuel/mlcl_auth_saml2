565542495aa2befd0209025fcfbd6673c6853bb8
Dominic Böttger <dominic.boettger@daslabor.rocks>
2016-10-26 19:42:20 +0200

    fixed url params and encoding

--- src/index.ts
+++ src/index.ts
@@ -104,7 +104,7 @@ class mlcl_auth_saml2 {
                     for(var i = 0; i < hashes.length; i++)
                     {
                         let hash = hashes[i].split('=');
-                        vars[hash[0]] = hash[1];
+                        vars[hash[0]] = decodeURIComponent(hash[1]);
                     }
                     return vars;
                 }
@@ -134,12 +134,56 @@ class mlcl_auth_saml2 {
               <head></head>
               <body>
                 <script>
+                  function addParameter(url, parameterName, parameterValue, atStart/*Add param before others*/){
+                      replaceDuplicates = true;
+                      if(url.indexOf('#') > 0){
+                          var cl = url.indexOf('#');
+                          urlhash = url.substring(url.indexOf('#'),url.length);
+                      } else {
+                          urlhash = '';
+                          cl = url.length;
+                      }
+                      sourceUrl = url.substring(0,cl);
+
+                      var urlParts = sourceUrl.split("?");
+                      var newQueryString = "";
+
+                      if (urlParts.length > 1)
+                      {
+                          var parameters = urlParts[1].split("&");
+                          for (var i=0; (i < parameters.length); i++)
+                          {
+                              var parameterParts = parameters[i].split("=");
+                              if (!(replaceDuplicates && parameterParts[0] == parameterName))
+                              {
+                                  if (newQueryString == "")
+                                      newQueryString = "?";
+                                  else
+                                      newQueryString += "&";
+                                  newQueryString += parameterParts[0] + "=" + (parameterParts[1]?parameterParts[1]:'');
+                              }
+                          }
+                      }
+                      if (newQueryString == "")
+                          newQueryString = "?";
+
+                      if(atStart){
+                          newQueryString = '?'+ parameterName + "=" + parameterValue + (newQueryString.length>1?'&'+newQueryString.substring(1):'');
+                      } else {
+                          if (newQueryString !== "" && newQueryString != '?')
+                              newQueryString += "&";
+                          newQueryString += parameterName + "=" + (parameterValue?parameterValue:'');
+                      }
+                      return urlParts[0] + newQueryString + urlhash;
+                  };
                   var params = JSON.parse(localStorage.getItem('samlparams'));
                   localStorage.removeItem('samlparams');
                   if(params) {
                     if(params.fwdurl) {`
-                      + 'window.location = params.fwdurl + \'?token=' + userObject.token + '\';' +
-                    `}
+                      + 'params.fwdurl = encodeURI(params.fwdurl);'
+                      + 'var url = addParameter(params.fwdurl,\'token\', \''+userObject.token+'\', false);'
+                      + 'window.location = url;'+
+                    `}  
                   }
                   localStorage.setItem('userData', `+ JSON.stringify(userObject) + `); 
                   console.log(`+JSON.stringify(userObject.token)+`);
